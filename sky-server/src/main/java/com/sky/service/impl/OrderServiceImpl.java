7e2a01fc1611d676631e666dfb6d7ce72c96ac49
root <3100672662@qq.com>
2025-11-12 14:21:51 +0800

    websocket实现paysuccess通知浏览器

--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.sky.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.sky.context.BaseContext;
 import com.sky.dto.OrdersPaymentDTO;
@@ -13,6 +14,7 @@ import com.sky.service.OrderService;
 import com.sky.utils.WeChatPayUtil;
 import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
+import com.sky.websocket.WebSocketServer;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,7 +23,9 @@ import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -37,6 +41,8 @@ public class OrderServiceImpl implements OrderService {
     UserMapper userMapper;
     @Autowired
     WeChatPayUtil weChatPayUtil;
+    @Autowired
+    WebSocketServer webSocketServer;
     @Override
     @Transactional
     public OrderSubmitVO order(OrdersSubmitDTO ordersSubmitDTO) {
@@ -130,5 +136,11 @@ public class OrderServiceImpl implements OrderService {
                 .build();
 
         orderMapper.update(orders);
+        Map<String,String> m=new HashMap<>();
+        m.put("type","1");
+        m.put("orderId",ordersDB.getId().toString());
+        m.put("content",outTradeNo);
+        String message=JSON.toJSONString(m);
+        webSocketServer.sendToAllClient(message);
     }
 }
